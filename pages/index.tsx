e017930c016729bed9695551581615f77ce42194
AndriusMrc <anmarciulionis@gmail.com>
2021-08-07 17:54:32 +1000

    [Housekeeping] Add Typescript to the project

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import { UseStateHook } from "../hooks/useState";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Learn React Hooks</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>
+        Learn React Hooks in
+        <a
+          href="https://github.com/AndriusMrc/react-hooks"
+          style={{ paddingLeft: "10px" }}
+        >
+          GitHub
+        </a>
+      </h1>
+      <br />
+      <h2>useEffect</h2>
+      <UseStateHook />
+      <hr style={{ width: "50%", marginLeft: "0" }} />
+    </div>
+  );
+};
+
+export default HomePage;
