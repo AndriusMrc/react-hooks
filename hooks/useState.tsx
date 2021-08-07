cebaf0d4db387d3a24f8fdee70b9c5bfc6ff72fe
AndriusMrc <anmarciulionis@gmail.com>
2021-08-07 17:56:47 +1000

    [Hooks] Update useState hook

--- hooks/useState.tsx
+++ hooks/useState.tsx
@@ -1,15 +1,14 @@
 /**
  * React will preserve this state between re-renders.
  * useState returns a pair of values: the current state value and a function that lets you update it. This is why we write const [count, setCount] = useState()
- * The only argument to useState is the initial state. In the example above, it is 0 because our counter starts from zero. It could be an object too.
- * If we wanted to store two different values in state, we would call useState() twice.
+ * The only argument to useState is the initial state. In the example below, it is 0 because our counter starts from zero. It could be an object too.
  * The initial state argument is only used during the first render.
+ * If we wanted to store two different values in state, we would call useState() twice.
  */
 
 import React, { useState } from "react";
 
-function UseStateHook() {
-  // Declear a new state variable, which we'll call "count"
+export const UseStateHook = () => {
   const [count, setCount] = useState(0);
   const [name, setName] = useState(undefined);
   const [todos, setTodos] = useState([{ text: "Learn Hooks" }]);
@@ -24,7 +23,7 @@ function UseStateHook() {
       <p>
         {name ? name : "You"} clicked {count} times
       </p>
-      <button onClick={() => handleButtonaClick}>Click me</button>
+      <button onClick={() => handleButtonaClick()}>Click me</button>
     </div>
   );
-}
+};
