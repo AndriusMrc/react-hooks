1ca476f3f91f091b0a8b5657a2f141afda825be4
AndriusMrc <anmarciulionis@gmail.com>
2021-08-10 21:36:33 +1000

    [Hooks] Update useState notes

--- hooks/useState.tsx
+++ hooks/useState.tsx
@@ -4,6 +4,11 @@
  * The only argument to useState is the initial state. In the example below, it is 0 because our counter starts from zero. It could be an object too.
  * The initial state argument is only used during the first render.
  * If we wanted to store two different values in state, we would call useState() twice.
+ *
+ * [Rendering]
+ * The setState function is used to update the state. It accepts a new state value and enqueues a re-render of the component.
+ * If you update a State Hook to the same value as the current state, React will bail out without rendering the children or firing effects. (React uses the Object.is comparison algorithm.)
+ * If you’re doing expensive calculations while rendering, you can optimize them with useMemo.
  */
 
 import React, { useState } from "react";
